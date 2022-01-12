7b8b2449e3817b38f80998de4593fe7cec6246ee
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-01-12 19:31:18 -0300

    importando componente e acessando propriedades

--- Aula/react-dio/src/App.js
+++ Aula/react-dio/src/App.js
@@ -1,6 +1,19 @@
+import Item from './components/Item';
+
+
+
 const App = () => {
   return(
-    <h1>Hello World</h1>
+    <>
+      <h1>Minha primeira aplicação com React</h1>
+      <ul>
+        {/* <Item texto='Item 1'/>
+        <Item texto='Item 2'/> */}
+        <Item>Item 1 </Item>
+        <Item>Item 2 </Item>
+        <Item>Item 3 </Item>
+      </ul>
+    </>
   )
 }
 
